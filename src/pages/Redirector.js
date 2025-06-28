5b35ba24727cafee9dce7168efc5ee296ec49690
Likitha Puvvula <likithapuvvula@gmail.com>
2025-06-28 12:25:47 +0530

    Update Redirector.js

--- src/pages/Redirector.js
+++ src/pages/Redirector.js
@@ -1,24 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getShortLink } from '../utils/storage';
-import { isExpired } from '../utils/timeUtils';
-import { logEvent } from '../utils/logger';
 import './Redirector.css';
-
 export default function Redirector() {
   const { shortcode } = useParams();
-  const [error, setError] = useState('');
 
   useEffect(() => {
-    const data = getShortLink(shortcode);
-    if (!data) return setError('Short link not found.');
-    if (isExpired(data.expiresAt)) return setError('This link has expired.');
-
-    data.clickCount += 1;
-    localStorage.setItem(`url_${shortcode}`, JSON.stringify(data));
-    logEvent('Redirect', { shortcode, longURL: data.longURL });
-    window.location.href = data.longURL;
+    const data = localStorage.getItem(`url_${shortcode}`);
+    if (data) {
+      const parsed = JSON.parse(data);
+      parsed.clickCount += 1;
+      localStorage.setItem(`url_${shortcode}`, JSON.stringify(parsed));
+      window.location.href = parsed.longURL;
+    } else {
+      alert('Invalid or expired link');
+    }
   }, [shortcode]);
 
-  return error ? <h2>{error}</h2> : <p>Redirecting...</p>;
+  return <p>Redirecting...</p>;
 }
